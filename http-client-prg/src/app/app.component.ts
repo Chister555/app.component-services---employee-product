ec825492d7eba99cb3b7975e664669e7fb373a3e
Chester Cruz <chestercomptia@gmail.com>
2025-02-09 10:43:45 +0800

    final http-client-prg

--- http-client-prg/src/app/app.component.ts
+++ http-client-prg/src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpclientService } from './httpclient.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'http-client-prg';
+  httpusers: any;
+
+  constructor(private httpclientlist: HttpclientService){
+    this.httpclientlist.getUsersRemotely().subscribe((data:any)=> {
+      this.httpusers = data
+    })
+  }
 }
